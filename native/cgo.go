b42619fa9f5151d0ff2d090d8c62905fead8dca6
Chih-Wei Fang <dodo0822@gmail.com>
2021-03-12 19:27:57 -0500

    add arm64 zlib

--- native/cgo.go
+++ native/cgo.go
@@ -3,7 +3,8 @@ package native
 /*
 #cgo CFLAGS: -I${SRCDIR}/zlib/
 #cgo windows LDFLAGS: ${SRCDIR}/libs/winlibz.a
-#cgo linux LDFLAGS: ${SRCDIR}/libs/linuxlibz.a
+#cgo linux,amd64 LDFLAGS: ${SRCDIR}/libs/linuxlibz.a
+#cgo linux,arm64 LDFLAGS: ${SRCDIR}/libs/linuxarm64libz.a
 #cgo darwin LDFLAGS: ${SRCDIR}/libs/darwinlibz.a
 */
 import "C"
